a3d826a574e6061bbc63c544a82a5efc831e903b
Guilherme Oliveira <melog0789@gmail.com>
2023-02-09 15:19:20 -0300

    Detail endpoint of a payment in the payments API

--- finance/src/controllers/pagamentoController.js
+++ finance/src/controllers/pagamentoController.js
@@ -6,12 +6,24 @@ class PagamentoController {
         const dados = req.body
         try{
             const novoPagamento = await database.payments.create(dados)
-            return res.status(201).json(`id: ${novoPagamento.dataValues.id}, status: Criado`)
+            return res.status(201)
+                .location('/payments/' + novoPagamento.dataValues.id)
+                .json(`id: ${novoPagamento.dataValues.id}, status: Criado`)
         }catch(err){
             return res.status(500).json(err.message)
         }
     }
-
+    
+    static async listarPagamentoPorId(req, res){
+        const {id} = req.params
+        try{
+            const dados = await database.payments.findOne({where: {id: Number(id)}})
+            const {cvv, ...pagamento} = dados.dataValues
+            return res.status(200).json(pagamento)
+        }catch(err){
+            return res.status(500).json(err.message)
+        }
+    }
 }
 
 module.exports = PagamentoController
--- finance/src/routes/pagamentosRoutes.js
+++ finance/src/routes/pagamentosRoutes.js
@@ -3,6 +3,7 @@ const PagamentoController = require('../controllers/pagamentoController.js')
 const router = express.Router()
 
 router
+    .get('/payments/:id', PagamentoController.listarPagamentoPorId)
     .post('/payments', PagamentoController.inserirPagamento)
 
 
