5a17a4998d393bc2f8ce34ef71c187d8ad40bd64
Guilherme Oliveira <melog0789@gmail.com>
2023-02-12 16:11:15 -0300

    Refactoring files

--- finance/src/routes/pagamentosRoutes.js
+++ finance/src/routes/pagamentosRoutes.js
@@ -3,9 +3,9 @@ const PagamentoController = require('../controllers/pagamentoController.js')
 const router = express.Router()
 
 router
-    .get('/payments/:id', PagamentoController.listarPagamentoPorId)
-    .post('/payments', PagamentoController.inserirPagamento)
-    .patch('/payments/:id', PagamentoController.respostaPagamento)
+    .get('/api/payments/:id', PagamentoController.listarPagamentoPorId)
+    .post('/api/payments', PagamentoController.inserirPagamento)
+    .patch('/api/payments/:id', PagamentoController.respostaPagamento)
   
 
 module.exports = router
