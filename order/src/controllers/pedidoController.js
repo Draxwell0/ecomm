794f363dabf59a31dd122a3de92952d757d4b169
Guilherme Oliveira <melog0789@gmail.com>
2023-02-14 12:22:50 -0300

    Fixing errors and inconsistencies

--- order/src/controllers/pedidoController.js
+++ order/src/controllers/pedidoController.js
@@ -7,6 +7,7 @@ class PedidoController {
 
         try{
             pedido.save(err=>{
+                if(err) res.status(500).send('Houve um erro ao inserir o pedido')
                 res.status(201).send(pedido.toJSON())
             })
         }catch(err){
@@ -20,34 +21,34 @@ class PedidoController {
 
         try{
             pedidos.findByIdAndUpdate(idPedido, {$set: {status: 'pago'}}, (err)=>{
-                if(err) throw new Error('Pedido não encontrado')
-
+                if(err) return res.status(404).send('O pedido inserido não existe')
                 res.status(200).send('Pedido pago')
             })
 
             pedidos.findById(idPedido, async (err, pedido)=>{
-
-                // obter o nome, CPF e endereço completo do cliente
-                const cliente = await fetch(`http://account:3002/api/admin/users/${pedido.idCliente}`)
-                const {nome, cpf, endereco} = await cliente.json()
-                
-                // obter o nome dos produtos, se necessário
-                const nomesProdutos = await Promise.all(
-                    pedido.itens.map(async (elm)=>{
-                        let produto = await fetch(`http://product:3001/api/products/${elm.id}`)
-                        produto = await produto.json()
-                        return produto.produto //model.produto = nome do produto
-                    })    
-                )
-                
-                // confirmar o pagamento passando todas as informações de produto para a geração da nota fiscal
-                await fetch(`http://finance:3003/api/payments/${idPagamento}?status=confirm`, {
-                    method: 'PATCH',
-                    body: JSON.stringify({nome, cpf, endereco, itens: pedido.itens}),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
+                if(!err){
+                    // obter o nome, CPF e endereço completo do cliente
+                    const cliente = await fetch(`http://account:3002/api/users/${pedido.idCliente}`)
+                    const {nome, cpf, endereco} = await cliente.json()
+                    
+                    // obter o nome dos produtos, se necessário
+                    const nomesProdutos = await Promise.all(
+                        pedido.itens.map(async (elm)=>{
+                            let produto = await fetch(`http://product:3001/api/products/${elm.id}`)
+                            produto = await produto.json()
+                            return produto.produto //model.produto = nome do produto
+                        })    
+                    )
+                    
+                    // confirmar o pagamento passando todas as informações de produto para a geração da nota fiscal
+                    await fetch(`http://finance:3003/api/payments/${idPagamento}?status=confirm`, {
+                        method: 'PATCH',
+                        body: JSON.stringify({nome, cpf, endereco, itens: pedido.itens}),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                } 
             })
         }catch(err){
             res.status(404).send(err.message)
--- order/src/models/Pedido.js
+++ order/src/models/Pedido.js
@@ -1,25 +1,25 @@
 import mongoose from 'mongoose'
 
 const itemSchema = new mongoose.Schema({
-        id: {type: String},
-        quantidade: {type: Number},
-        desconto: {type: Number},
-        precoUnitario: {type: Number},
+        id: {type: String, required: true},
+        quantidade: {type: Number, required: true},
+        desconto: {type: Number, required: true},
+        precoUnitario: {type: Number, required: true},
 })
 
 const pedidoSchema = new mongoose.Schema({
     id: {type: String},
-    idCliente: {type: mongoose.Types.ObjectId},
-    status: {type: String, default: 'realizado'},
+    idCliente: {type: mongoose.Types.ObjectId, required: true},
+    status: {type: String, default: 'realizado', required: true},
     enderecoEntrega: {
-        rua: {type: String},
-        numero: {type: String},
-        complemento: {type: String},
-        cep: {type: String},
-        cidade: {type: String},
-        estado: {type: String},
+        rua: {type: String, required: true},
+        numero: {type: String, required: true},
+        complemento: {type: String, required: true},
+        cep: {type: String, required: true},
+        cidade: {type: String, required: true},
+        estado: {type: String, required: true},
     },
-    itens: {type: [itemSchema]}
+    itens: {type: [itemSchema], required: true}
 }, {versionKey: false})
 
 const pedido = mongoose.model('orders', pedidoSchema);
