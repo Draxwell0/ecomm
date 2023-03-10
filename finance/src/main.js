c88527a5b42aa1ac82975338b3f9b1ae89b2c9c9
Guilherme Oliveira <melog0789@gmail.com>
2023-03-10 10:14:09 -0300

    Authorization in the payments API

--- finance/src/main.js
+++ finance/src/main.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes/index.js');
 
+require('./utils/estrategiasAutenticacao.js');
+
 const app = express();
 app.use(express.json());
 routes(app);
